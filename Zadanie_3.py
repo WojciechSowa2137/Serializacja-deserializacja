6aa0577fc129f8b2019beef65d12d6a15f0b1fbc
Wojciech Sowa <u7_wojsow_lbn@technischools.com>
2023-12-07 18:25:16 +0100

    add

---
+++ Zadanie_3.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+
+
+class ProductSchema(Schema):
+    id = fields.UUID(required=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    producer_mail = fields.Email(required=True)
+    type = fields.String()
+
+
+json_data = {
+    "id": 2, "name": "Madra ksiazka 1", "price": 3.23, "producer_mail": "fds@fds.com", "type": "nowy2"
+}
